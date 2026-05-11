94704ca43bafdd431a41d9ad4d94cf02ff0d7f88
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-05-11 12:59:15 +0200

    feature: Añadir oferta 'Sale'

---
+++ app/src/main/java/edu/teamrocket/stockx/offer/Sale.java
@@ -0,0 +1,34 @@
+package edu.teamrocket.stockx.offer;
+
+class Sale implements Offer {
+    private final String size;
+    private final int sale;
+
+    Sale(String size, int sale) {
+        this.size = size;
+        this.sale = sale;
+    }
+
+    @Override
+    public String size() {
+        return this.size;
+    }
+
+    @Override
+    public int value() {
+        return this.sale;
+    }
+
+    @Override
+    public int compareTo(Offer offer) {
+        return this.value() - offer.value();
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Size: \s
+                Value: \n
+                """.formatted(this.size(), this.value());
+    }
+}
