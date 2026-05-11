7876e8c14235d3c1f508c28a6d4dbd329235e174
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-05-11 12:57:40 +0200

    feature: Añadir oferta 'Ask'

---
+++ app/src/main/java/edu/teamrocket/stockx/offer/Ask.java
@@ -0,0 +1,34 @@
+package edu.teamrocket.stockx.offer;
+
+class Ask implements Offer {
+    private final String size;
+    private final int ask;
+
+    Ask(String size, int ask) {
+        this.size = size;
+        this.ask = ask;
+    }
+
+    @Override
+    public String size() {
+        return this.size;
+    }
+
+    @Override
+    public int value() {
+        return this.ask;
+    }
+
+    @Override
+    public int compareTo(Offer offer) {
+        return this.value() - offer.value();
+    }
+
+    @Override
+    public String toString() {
+        return """
+                Size: \s
+                Value: \n
+                """.formatted(this.size(), this.value());
+    }
+}
