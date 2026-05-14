75686c85790f41a261dae0eaba0b79289bb87327
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2026-05-14 09:11:56 +0200

    test: Actualizar importaciones de los test para adaptarlo a junit 5

--- app/src/test/java/edu/teamrocket/stockx/offer/SaleTest.java
+++ app/src/test/java/edu/teamrocket/stockx/offer/SaleTest.java
@@ -1,7 +1,7 @@
 package edu.teamrocket.stockx.offer;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class SaleTest {
     
